f697c6e9915de1c76e6ced1af162d2015f118246
Philip Crockett <contact@philcrockett.com>
2021-01-23 16:45:56 +0100

    Unnecessary group commands

--- setup.d/05_unprivileged-user.sh
+++ setup.d/05_unprivileged-user.sh
@@ -7,8 +7,6 @@ echo "Creating user ${UNPRIVILEGED_USER}..."
 if is_root; then
     useradd --create-home --shell /usr/bin/bash "${UNPRIVILEGED_USER}"
     passwd "${UNPRIVILEGED_USER}"
-    groupadd --system "${UNPRIVILEGED_USER}"
-    gpasswd --add "${UNPRIVILEGED_USER}" "${UNPRIVILEGED_USER}"
 else
-    sudo --preserve-env "$(readlink -f "${0}")"
+    sudo --preserve-env=UNPRIVILEGED_USER "$(readlink -f "${0}")"
 fi
