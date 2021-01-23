8265fbaa2e72885e327a4fb43a85198d2e1a8d7b
Philip Crockett <contact@philcrockett.com>
2021-01-23 17:03:26 +0100

    SSH user settings

--- setup.d/10_user-ssh-settings.sh
+++ setup.d/10_user-ssh-settings.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-echo "TODO: Implement user-ssh-settings"
+unprivileged_ssh_dir="/home/${UNPRIVILEGED_USER}/.ssh"
+
+if is_root; then
+    if [ ! -f ~/.ssh/authorized_keys ]; then
+        mkdir --parent ~/.ssh
+        echo "${ROOT_SSH_KEY}" > ~/.ssh/authorized_keys
+    fi
+
+    if [ ! -f "${unprivileged_ssh_dir}/authorized_keys" ]; then
+        sudo --login --user "${UNPRIVILEGED_USER}" \
+            --preserve-env=UNPRIVILEGED_USER,UNPRIVILEGED_SSH_KEY \
+            "$(readlink -f "${0}")"
+    fi
+else
+    mkdir --parent "${unprivileged_ssh_dir}"
+    echo "${UNPRIVILEGED_SSH_KEY}" > "${unprivileged_ssh_dir}/authorized_keys"
+    chmod -R go-rwx "${unprivileged_ssh_dir}"
+fi
