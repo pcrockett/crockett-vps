0e9e1ed581195bf21a46fc487c07a73e26f0cc13
Philip Crockett <contact@philcrockett.com>
2021-01-23 17:50:58 +0100

    Fix typos

--- setup.d/15_server-ssh-settings.sh
+++ setup.d/15_server-ssh-settings.sh
@@ -2,7 +2,7 @@
 
 test_checkpoint "server-ssh-settings" || return 0
 
-echo "Settings up SSH server settings..."
+echo "Setting up SSH server settings..."
 
 # Remove all default host keys and regenerate from scratch
 rm /etc/ssh/ssh_host_*key*
@@ -10,8 +10,8 @@ ssh-keygen -t rsa -b 4096 -f "/etc/ssh/ssh_host_rsa_key" -N "" < /dev/null
 ssh-keygen -t ed25519 -f "/etc/ssh/ssh_host_ed25519_key" -N "" < /dev/null
 
 groupadd ssh-user
-usermod --apend --groups ssh-user "${UNPRIVILEGED_USER}"
-usermod --apend --groups ssh-user root
+usermod --append --groups ssh-user "${UNPRIVILEGED_USER}"
+usermod --append --groups ssh-user root
 
 place_file "etc/ssh/sshd_config"
 
