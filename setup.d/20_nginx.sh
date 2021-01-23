286b743d8e4b8b9c3cd163c5a6a6da318eeb185d
Philip Crockett <contact@philcrockett.com>
2021-01-23 21:29:15 +0100

    Start and enable nginx

--- setup.d/20_nginx.sh
+++ setup.d/20_nginx.sh
@@ -8,3 +8,6 @@ is_installed nginx || install_nginx
 
 test_checkpoint "nginx-conf" || place_file "etc/nginx/nginx.conf"
 set_checkpoint "nginx-conf"
+
+systemctl status nginx || systemctl start nginx
+systemctl enable nginx
