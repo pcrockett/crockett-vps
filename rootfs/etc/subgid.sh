6b486d26f1efebaf5f86af01e0721a955b70b5b6
Philip Crockett <contact@philcrockett.com>
2021-01-24 14:25:38 +0100

    Set up subuid and subgid

---
+++ rootfs/etc/subgid.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat << EOF
+${UNPRIVILEGED_USER}:100000:65536
+EOF
--- setup.d/30_podman.sh
+++ setup.d/30_podman.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+function file_contains() {
+    test "${#}" -eq 2 || panic "Expecting two arguments: pattern and file path"
+
+    local pattern="${1}"
+    local file_path="${2}"
+
+    if [ -f "${file_path}" ]; then
+        grep "${pattern}" "${file_path}" > /dev/null
+    else
+        false # File does not exist in the first place
+    fi
+
+}
+
+if file_contains "${UNPRIVILEGED_USER}" "/etc/subuid"; then
+    place_template "etc/subuid"
+fi
+
+if file_contains "${UNPRIVILEGED_USER}" "/etc/subgid"; then
+    place_template "etc/subgid"
+fi
+
 function install_podman() {
     yes | pacman --sync crun podman
 }
