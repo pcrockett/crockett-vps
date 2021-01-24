0175df7b45a04135093c198d223fde03b1c86023
Philip Crockett <contact@philcrockett.com>
2021-01-24 14:41:43 +0100

    Unimplemented Dendrite script

---
+++ setup.d/35_dendrite-image.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# TODO: Build a Dendrite image and run it.
+#
+#     https://github.com/matrix-org/dendrite/tree/master/build/docker
+#
+# Either we can do something manual, potentially involving this command...
+#
+#    podman build --tag dendrite SOME_DOCKERFILE
+#
+# ... or perhaps https://github.com/containers/podman-compose will work well
+# enough for us.
