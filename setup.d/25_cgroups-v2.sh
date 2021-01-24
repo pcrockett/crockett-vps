e631966f914247b28794aef3224e2b6f1b584c5a
Philip Crockett <contact@philcrockett.com>
2021-01-24 13:17:08 +0100

    Enable v2 cgroups via grub

--- setup.d/25_cgroups-v2.sh
+++ setup.d/25_cgroups-v2.sh
@@ -4,8 +4,31 @@ test ! -f /sys/fs/cgroup/cgroup.controllers || return 0
 
 # If the cgroup.controllers file doesn't exit, then we're using cgroups v1. Let's change that.
 if is_unset_checkpoint "v2-cgroups-enabled"; then
-    new_cmdline="$(cat /proc/cmdline) systemd.unified_cgroup_heirarchy=1"
-    echo "${new_cmdline}" > /proc/cmdline
+
+    grub_config_file="/etc/default/grub"
+
+    current_cmdline="$(grep "^GRUB_CMDLINE_LINUX_DEFAULT=\".*\"\$" "${grub_config_file}")"
+
+    # ${current_cmdline} probably looks something like this:
+    #
+    #     GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3"
+    #
+
+    # shellcheck disable=SC2001
+    new_cmdline="$(echo "${current_cmdline}" | sed "s/\"$/ systemd.unified_cgroup_heirarchy=1\"/")"
+    # Disabled SC2001 because we need more advanced functionality of sed (finding the last `"` character)
+
+    # ${new_cmdline} probably looks something like this:
+    #
+    #     GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 systemd.unified_cgroup_heirarchy=1"
+    #
+
+    # Now simply replace the current cmdline with the new cmdline
+    sed --in-place "s/${current_cmdline}/${new_cmdline}/" "${grub_config_file}"
+
+    # Rebuild Grub boot config
+    grub-mkconfig --output /boot/grub/grub.cfg
+
     set_checkpoint "v2-cgroups-enabled"
 fi
 
