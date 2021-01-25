a1bd93d9caa7dc65e60de61ef9ac86ead71c2022
Philip Crockett <contact@philcrockett.com>
2021-01-25 16:31:33 +0100

    Generate keys for Dendrite

--- setup.d/45_dendrite-image.sh
+++ setup.d/45_dendrite-image.sh
@@ -4,20 +4,35 @@
 #
 #     https://github.com/matrix-org/dendrite/tree/master/build/docker
 #
-# Either we can do something manual, potentially involving this command...
-#
-#    podman build --tag dendrite SOME_DOCKERFILE
-#
-# ... or perhaps https://github.com/containers/podman-compose will work well
-# enough for us.
+# The following is a work in progress.
 
 image_name="docker.io/matrixdotorg/dendrite-monolith:latest"
 container_name="dendrite"
+container_data_dir="/etc/dendrite" # The path where Dendrite data is stored inside the container
+volume="dendrite-data:${container_data_dir}"
 
-run_unprivileged podman container exists "${container_name}" \
-    || run_unprivileged podman container create \
+if run_unprivileged podman container exists "${container_name}"; then
+    run_unprivileged podman container start "${container_name}"
+else
+
+    # First run. Generate keys and save them in the dendrite-data volume.
+    run_unprivileged podman container run \
+        --entrypoint /usr/bin/generate-keys \
+        --volume "${volume}" \
+        "${image_name}" \
+        "--private-key=${container_data_dir}/matrix_key.pem" \
+        "--tls-cert=${container_data_dir}/server.crt" \
+        "--tls-key=${container_data_dir}/server.key"
+
+    # TODO: Place Dendrite config file in the volume as well.
+
+    # Now create the actual container where Dendrite will run
+    run_unprivileged podman container create \
         --name "${container_name}" \
         --publish 8008:8008 \
         --publish 8448:8448 \
-        --volume "dendrite-data:/etc/dendrite" \
+        --volume "${volume}" \
         "${image_name}"
+
+    run_unprivileged podman container start "${container_name}"
+fi
