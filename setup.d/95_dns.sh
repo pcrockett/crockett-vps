b45beb6103565b24fc34cad663732241ac8962ae
Philip Crockett <contact@philcrockett.com>
2021-01-31 21:58:00 +0100

    Provide DNS service to WireGuard peers

--- setup.d/95_dns.sh
+++ setup.d/95_dns.sh
@@ -5,4 +5,10 @@ is_unset_checkpoint "dns-settings" || return 0
 place_file "etc/systemd/resolved.conf"
 systemctl restart systemd-resolved
 
+# We would be fine with just systemd-resolved, however we want to provide DNS
+# to the peers who join our WireGuard VPN. That's not what systemd-resolved is
+# for. Install dnsmasq for that task.
+is_installed dnsmasq || install_package dnsmasq
+enable_and_start dnsmasq
+
 set_checkpoint "dns-settings"
