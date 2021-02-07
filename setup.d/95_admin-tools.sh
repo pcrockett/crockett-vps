a94af9b137051d0adabbee9e119af5060baadb48
Philip Crockett <contact@philcrockett.com>
2021-02-07 16:48:19 +0100

    Make admin tools available on PATH

---
+++ setup.d/95_admin-tools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source_dir="$REPO_ROOT/admin-tools"
+bin_dir="/usr/local/bin"
+
+function put_admin_tool() {
+    ln --symbolic "${source_dir}/${1}.sh" "${bin_dir}/${1}"
+}
+
+is_installed new-matrix-user || put_admin_tool new-matrix-user
+is_installed new-wireguard-peer || put_admin_tool new-wireguard-peer
