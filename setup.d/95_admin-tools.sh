33ddaaaaa55be7495eed80fe3d03630421d8e006
Philip Crockett <contact@philcrockett.com>
2021-02-07 16:53:46 +0100

    Make "run" accessible as "server-cmd"

--- setup.d/95_admin-tools.sh
+++ setup.d/95_admin-tools.sh
@@ -9,3 +9,4 @@ function put_admin_tool() {
 
 is_installed new-matrix-user || put_admin_tool new-matrix-user
 is_installed new-wireguard-peer || put_admin_tool new-wireguard-peer
+is_installed server-cmd || put_admin_tool server-cmd
