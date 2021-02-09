8682ea9ec83bdab070afd3bc9fb5b29e066fb083
Philip Crockett <contact@philcrockett.com>
2021-02-09 20:16:36 +0100

    Install yq

--- setup.d/70_misc-admin-tools.sh
+++ setup.d/70_misc-admin-tools.sh
@@ -2,3 +2,6 @@
 
 # pacdiff and checkupdates are useful tools from pacman-contrib
 is_installed pacdiff || install_package pacman-contrib
+
+# yq to parse the Arch news feed before auto updates
+is_installed yq || install_package yq
