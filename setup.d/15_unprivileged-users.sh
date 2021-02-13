087740638702b11b5b7cec538c63a3763c84d26a
Philip Crockett <contact@philcrockett.com>
2021-02-13 22:47:06 +0100

    Fix podman permissions warning

--- setup.d/15_unprivileged-users.sh
+++ setup.d/15_unprivileged-users.sh
@@ -3,4 +3,7 @@
 for user in "${UNPRIVILEGED_USERS[@]}"
 do
     test -d "/home/${user}" || create_user "${user}"
+
+    # Start a user manager service for this user at boot. Podman needs this.
+    loginctl enable-linger "${user}"
 done
