41ffa8f3a50232fab90a8be37f6712295d2851b2
Philip Crockett <contact@philcrockett.com>
2021-02-14 12:35:43 +0100

    Create autoupdate-prep timer

--- setup.d/75_autoupdate.sh
+++ setup.d/75_autoupdate.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 install_and_enable_timer autoupdate
-
-# TODO: Once autoupdate is implemented, we need to add a `checkupdates` and
-# `checknews` timer that runs Sunday morning and warns us about issues that
-# might plague our upcoming autoupdate.
+install_and_enable_timer autoupdate-check
