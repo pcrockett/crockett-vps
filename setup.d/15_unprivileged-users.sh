7582956c24b3f54c27eb6c049ec9ba21dfe311de
Philip Crockett <contact@philcrockett.com>
2021-04-10 12:17:49 +0200

    Fix storage config problems in podman

--- setup.d/15_unprivileged-users.sh
+++ setup.d/15_unprivileged-users.sh
@@ -4,6 +4,8 @@ for user in "${UNPRIVILEGED_USERS[@]}"
 do
     test -d "/home/${user}" || create_user "${user}"
 
+    place_file "home/${user}/.config/containers/storage.conf"
+
     # Start a user manager service for this user at boot. Podman needs this.
     loginctl enable-linger "${user}"
 done
