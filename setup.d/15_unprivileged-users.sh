5b957b4406d320cad854fce77477ed62ad62e9f5
Philip Crockett <contact@philcrockett.com>
2021-06-08 08:21:36 +0200

    Fix bug causing .config dirs to be owned by root

--- setup.d/15_unprivileged-users.sh
+++ setup.d/15_unprivileged-users.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
+function mkdir_user() {
+    test "${#}" -eq 2 || panic "Expecting 2 arguments: directory path and owning user"
+    local path="${1}"
+    local user="${2}"
+
+    if [ ! -d "${path}" ]; then
+        mkdir "${path}"
+        chown "${user}:${user}" "${path}"
+    fi
+}
+
 for user in "${UNPRIVILEGED_USERS[@]}"
 do
-    test -d "/home/${user}" || create_user "${user}"
+    user_home_dir="home/${user}" # Intentionally leaving off first `/`
 
-    place_file "home/${user}/.config/containers/storage.conf"
+    test -d "/${user_home_dir}" || create_user "${user}"
+
+    config_dir="${user_home_dir}/.config"
+    mkdir_user "/${config_dir}" "${user}"
+
+    container_conf_dir="${config_dir}/containers"
+    mkdir_user "/${container_conf_dir}" "${user}"
+
+    place_file "${container_conf_dir}/storage.conf"
 
     # Start a user manager service for this user at boot. Podman needs this.
     loginctl enable-linger "${user}"
